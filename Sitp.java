ff0893e0ac50e8f7e53d415586a3dd0f5cb29238
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-13 17:54:28 -0500

    Sitp 1.0.3
    SE agregas todas las estaciones a sus respectivos buses

--- Sitp.java
+++ Sitp.java
@@ -4,7 +4,7 @@ public class Sitp { //NOmbre del Archivo
 
     public static void main(String[] args) {
         // Tu código comienza aquí
-        Scanner Scanner = new S(System.in); // Para que lea los inputs
+        Scanner Scanner = new Scanner(System.in); // Para que lea los inputs
 
                 // Crear instancias de las clases Bus y RutaFeriado
                 Bus Transmi1 = new Bus("G43");
@@ -13,9 +13,13 @@ public class Sitp { //NOmbre del Archivo
                 RutaFeriado rutaFeriado = new RutaFeriado();
 
                 // Agregar estaciones a los buses
-                Transmi1.agregarEstacion("Estacion1");
-                Transmi1.agregarEstacion("Estacion2");
-                Transmi1.agregarEstacion("Estacion3");
+                Transmi1.agregarEstacion("Marly");
+                Transmi1.agregarEstacion("Heroes");
+                Transmi1.agregarEstacion("Portal el dorado");
+
+                Transmi2.agregarEstacion("Heroes");
+                Transmi2.agregarEstacion("El dorado");
+                Transmi2.agregarEstacion("Usme");
 
     }
 }
