b223d1d06e84881b926541fe16779bbdb4606306
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-13 17:06:16 -0500

    Sistema_Sitp
    Cree la clase Sitp y las variables de Trasmi 1, 2 y rutas feriado

---
+++ Sitp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sitp { //NOmbre del Archivo
+
+    public static void main(String[] args) {
+        // Tu código comienza aquí
+        Scanner Scanner = new S(System.in); // Para que lea los inputs
+
+                // Crear instancias de las clases Bus y RutaFeriado
+                Bus Transmi1 = new Bus("G43");
+                Bus Transmi2 = new Bus("A21");
+
+                RutaFeriado rutaFeriado = new RutaFeriado();
+
+    }
+}
