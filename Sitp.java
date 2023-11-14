4a5d8bc399e1da5e119706eec20721ccbef1f833
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-14 00:48:09 -0500

    Sitp 1.0.4
    Comienzo el while para crear el menu de busqueda

--- Sitp.java
+++ Sitp.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Sitp { //NOmbre del Archivo
 
     public static void main(String[] args) {
-        // Tu código comienza aquí
+
         Scanner Scanner = new Scanner(System.in); // Para que lea los inputs
 
                 // Crear instancias de las clases Bus y RutaFeriado
                 Bus Transmi1 = new Bus("G43");
                 Bus Transmi2 = new Bus("A21");
-
+                // Bus es la clase Y transmi son las variables
                 RutaFeriado rutaFeriado = new RutaFeriado();
 
                 // Agregar estaciones a los buses
@@ -18,8 +18,20 @@ public class Sitp { //NOmbre del Archivo
                 Transmi1.agregarEstacion("Portal el dorado");
 
                 Transmi2.agregarEstacion("Heroes");
-                Transmi2.agregarEstacion("El dorado");
-                Transmi2.agregarEstacion("Usme");
+                Transmi2.agregarEstacion("Tunal");
+                Transmi2.agregarEstacion("Portal del Norte");
 
+                //Agrego las rutas en feriads
+                rutaFeriado.agregarRutaFeriado("G43");
+
+                //Menu
+                int Opcion;
+                
+                System.out.println("quiere iniciar la app");
+
+                while (1) {
+                    
+                }
+                
     }
 }
